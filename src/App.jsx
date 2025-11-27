dcefe09561979514a62ec796bd632a35287177b9
Kevin Moses <kevin.moses@yale.edu>
2025-11-26 21:35:22 -0500

    fix scroll issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import ScrollToTop from './components/ScrollToTop';
 import LandingPage from './pages/LandingPage';
 import ProjectPage from './pages/ProjectPage';
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/projects/:slug" element={<ProjectPage />} />
@@ -15,4 +17,3 @@ function App() {
 }
 
 export default App;
-
